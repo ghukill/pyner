7a25f2b1ea8c7f3f07f00cfa2c25dd350dd7b64d
Graham Hukill <ghukill@gmail.com>
2017-06-05 16:31:40 -0400

    organizing

--- pyner.py
+++ pyner.py
@@ -1,3 +1,4 @@
+import csv
 import time
 
 from nltk.tag.stanford import StanfordNERTagger
@@ -24,29 +25,89 @@ class TaggedDoc(object):
 
 		# file path provided
 		if self.payload_type == 'file':
-			with open(self.file_path, 'r') as f:
+			with open(self.payload, 'r') as f:
 				self.tags = st.tag(f.read().split())
 		# assume raw text
 		else:
 			self.tags = st.tag(self.payload.split())
 
 		# parse and save
+
+		'''
+		What we need to do here is group terms that come together.  For example:
+
+		 (u'Federal', u'ORGANIZATION'),
+		 (u'Reserve', u'ORGANIZATION'),
+		 (u'Bank', u'ORGANIZATION'),
+		 (u'of', u'ORGANIZATION'),
+		 (u'New', u'ORGANIZATION'),
+		 (u'York', u'ORGANIZATION'),
+		 (u'and', u'O'),
+		 (u'led', u'O'),
+		 (u'by', u'O'),
+		 (u'Timothy', u'PERSON'),
+		 (u'R.', u'PERSON'),
+		 (u'Geithner,', u'PERSON'),
+
+		 we should save "Federal Reserve Bank of New York" and "Timothy R. Geithner"
+		'''
+
+		# group tags
+		self.tags = self._group_consecutives(self.tags)
+
+		# parse
 		for tag in self.tags:
 
 			# locations
 			if tag[1] == 'LOCATION':
-				self.locations.append(tag[0])
+				self.locations.append(" ".join(tag[0]))
 
 			# people
 			if tag[1] == 'PERSON':
-				self.people.append(tag[0])
+				self.people.append(" ".join(tag[0]))
 
 			# organization
 			if tag[1] == 'ORGANIZATION':
-				self.organizations.append(tag[0])
+				self.organizations.append(" ".join(tag[0]))
 
 		# report
 		print "took %ss to analyze" % (time.time()-stime)
+
+
+	def _group_consecutives(self, tags):
+		run = []
+		result = []
+		expect = None
+		last_tag = None
+		for value,tag in tags:
+			if tag in ['PERSON','LOCATION','ORGANIZATION']:
+				last_tag = tag
+				if (tag == expect) or (expect is None):
+					run.append(value)
+					expect = tag
+				else:
+					run = [value]
+					result.append((run,last_tag))
+			else:
+				if len(run) > 0:
+					result.append((run,last_tag))
+				expect = None
+				run = []
+		if len(run) > 0:
+			result.append((run,last_tag))
+		return result
+
+
+	def write_csv(self, output_file_path):
+
+		with open(output_file_path,'w') as f:
+			writer = csv.writer(f, delimiter=',')
+			for location in set(self.locations):
+				writer.writerow([location,'LOCATION'])
+			for person in set(self.people):
+				writer.writerow([person,'PERSON'])	
+			for organization in self.organizations:
+				writer.writerow([organization,'ORGANIZATION'])
 
 
 
